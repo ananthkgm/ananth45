1dcc05c85d721b4ba36607b2297b0a6f51f2685a
ananthkgm <35257712+ananthkgm@users.noreply.github.com>
2018-01-19 16:32:30 +0530

    Create minele.c

---
+++ minele.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a[50],i,n,min;
+printf("enter the range");
+scanf("%d",&n);
+printf("enter the array values");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+min=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]<min)
+{
+min=a[i];
+}
+}
+printf("%d",min);
+}
