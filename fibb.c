c0ffbaa5364f2c65e3d531e52ac41587e832a093
ananthkgm <35257712+ananthkgm@users.noreply.github.com>
2018-01-23 16:48:21 +0530

    Update fibb.c

--- fibb.c
+++ fibb.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 {
-int sum=0,num1,num2,i,n;
-scanf("%d%d%d",&num1,&num2,&n);
+int sum=0,no1,no2,i,n;
+scanf("%d%d%d",&no1,&no2,&n);
 for(i=0;i<n;i++)
 {
-sum=num1+num2;
+sum=no1+no2;
 printf("%d\n",sum);
-num1=num2;
-num2=sum;
+no1=no2;
+no2=sum;
 }
 return 0;
 }
