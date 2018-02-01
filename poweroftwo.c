8697b49ccef42e7b278027db9a15259bd2817aae
ananthkgm <35257712+ananthkgm@users.noreply.github.com>
2018-02-01 14:20:51 +0530

    Create poweroftwo.c

---
+++ poweroftwo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\nEnter the number:");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("it is power of two");
+}
+else
+{
+printf("it is not power of two");
+}
+return 0;
+}
