959092d639f923f095665c30cebe7a223f6b4cd2
ananthkgm <35257712+ananthkgm@users.noreply.github.com>
2018-02-02 14:53:37 +0530

    Update evennum.c

--- evennum.c
+++ evennum.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int num;
+	int num,i=0;
 	printf("enter the number:\n");
 	scanf("&d",&num);
+	for(i=num;i>0;i--)
 	{
 		if(num%2==0)
 		{
 			printf("%d",num);
+			break;
 		}
-		return 0;
-   } 
+         }
+	return 0;
 }
+
